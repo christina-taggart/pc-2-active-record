b2f4ff33f9b6e05a5f768d12177ea52df2518d0f
John Olmsted <johnaolmsted@gmail.com>
2014-02-01 19:56:28 -0800

    Add #proficiency_default and #proficiency_for methods

--- source/ar-people-skills/app/models/user.rb
+++ source/ar-people-skills/app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :user_skills
   has_many :skills, through: :user_skills
+
+  def proficiency_for(skill)
+    tested_user_skill = UserSkill.where(user_id: self.id, skill_id: skill.id).first
+    tested_user_skill.proficiency
+  end
 end
--- source/ar-people-skills/app/models/user_skill.rb
+++ source/ar-people-skills/app/models/user_skill.rb
@@ -1,4 +1,11 @@
 class UserSkill < ActiveRecord::Base
   belongs_to :user
   belongs_to :skill
+  before_save :proficiency_default
+
+  private
+
+  def proficiency_default
+    self.proficiency = 0
+  end
 end
