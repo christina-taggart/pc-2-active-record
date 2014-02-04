347ef6a99173b94c5949ccbc174669583fe511e9
Elliot Chalfant <epchalfant@gmail.com>
2014-02-04 09:11:00 -0800

    saving work

--- source/ar-people-skills/db/migrate/20140203221701_create_skills_users.rb
+++ source/ar-people-skills/db/migrate/20140203221701_create_skills_users.rb
@@ -1,4 +1,11 @@
 class CreateSkillsUsers < ActiveRecord::Migration
   def change
+  	create_table :skills_users do |t|
+  		t.string :proficienty_rating
+  		t.belongs_to :skill
+  		t.belongs_to :user
+
+  		t.timestamps
+  	end
   end
 end
