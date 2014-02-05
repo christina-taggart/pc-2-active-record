32c2f824af045dc1eff0ca1192700f5c4cfaceef
Ricardo Rubio <rickarubio@gmail.com>
2014-02-04 23:28:48 -0800

    Added proficiency_for and set_proficiency_for methods to the User model

--- source/ar-people-skills/app/models/user.rb
+++ source/ar-people-skills/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_many :ratings
   has_many :skills, :through => :ratings
+  def proficiency_for(skill)
+    self.ratings.where(skill_id: skill.id).first.proficiency_rating
+  end
+
+  def set_proficiency_for(skill, rating)
+    self.ratings.where(skill_id: skill.id).first.update_attributes({proficiency_rating: rating})
+  end
 end
