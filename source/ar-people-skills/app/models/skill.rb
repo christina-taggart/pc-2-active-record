56a72820a002ac5926d6b8b8f9a10057c51ea182
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-04 20:15:40 -0800

    Add #user_with_proficiency and #set_proficiency_for methods, all six tests now pass

--- source/ar-people-skills/app/models/skill.rb
+++ source/ar-people-skills/app/models/skill.rb
@@ -4,4 +4,12 @@ class Skill < ActiveRecord::Base
 
   validates :name, uniqueness: true
 
+  def user_with_proficiency(proficiency)
+  	skill_id = self.id
+  	abilities_with_this_proficiency_and_skill = Ability.where(proficiency_rating: proficiency).where(skill_id: skill_id)
+  	abilities_with_this_proficiency_and_skill.map! {|ability| ability[:user_id]}
+  	abilities_with_this_proficiency_and_skill.map! {|user_id| User.find(user_id)}
+  	abilities_with_this_proficiency_and_skill[0]
+  end
+
 end
--- source/ar-people-skills/app/models/user.rb
+++ source/ar-people-skills/app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
   	skill_id = skill.id
   	Ability.where(user_id: user_id).where(skill_id: skill_id)[0][:proficiency_rating].to_i
   end
+
+  def set_proficiency_for(skill, proficiency_rating)
+  	user_id = self.id
+  	skill_id = skill.id
+  	Ability.where(user_id: user_id).where(skill_id: skill_id)[0].update_attributes(:proficiency_rating => proficiency_rating)
+  end
 end
