1db832846b6ac8eb2953c2b537be9bfa789b9bcf
Ricardo Rubio <rickarubio@gmail.com>
2014-02-04 23:08:03 -0800

    removed null constraint from CreateRatings migration proficiency_rating column, gave it a default value of 0 instead

--- source/ar-people-skills/db/migrate/20140204210013_create_ratings.rb
+++ source/ar-people-skills/db/migrate/20140204210013_create_ratings.rb
@@ -3,7 +3,7 @@ class CreateRatings < ActiveRecord::Migration
     create_table :ratings do |t|
       t.belongs_to :user, null: false
       t.belongs_to :skill, null: false
-      t.float :proficiency_rating, null: false
+      t.float :proficiency_rating, default: 0.0
       t.timestamps
     end
   end
