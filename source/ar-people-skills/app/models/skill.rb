d567e3f02f567ba801a561d3d06814add638e371
Ricardo Rubio <rickarubio@gmail.com>
2014-02-05 20:13:01 -0800

    added uniqueness validation to Skill model and removed extraneous comments

--- source/ar-people-skills/app/models/skill.rb
+++ source/ar-people-skills/app/models/skill.rb
@@ -1,15 +1,8 @@
 class Skill < ActiveRecord::Base
   has_many :ratings
   has_many :users, :through => :ratings
+  validates :name, :uniqueness => true
   def user_with_proficiency(rating)
-    
+
   end
 end
-
-
-# true Zee has the beard growing skill!
-# true Zee can Programm!!!!!!
-# true Jesse has the beard growing skill!
-# false Second Beard Growing Skill is not saved
-# true  Jesse cannot grow a beard :(
-# false ! Zee is a Beard Growing MASTER
