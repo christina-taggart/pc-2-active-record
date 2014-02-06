e3e9026fd0b788ab41d902f8c9f9d4ef57bb7c49
Ricardo Rubio <rickarubio@gmail.com>
2014-02-05 20:32:58 -0800

    Implemented Skill#user_with_proficiency_rating(rating)

--- source/ar-people-skills/app/models/skill.rb
+++ source/ar-people-skills/app/models/skill.rb
@@ -3,6 +3,7 @@ class Skill < ActiveRecord::Base
   has_many :users, :through => :ratings
   validates :name, :uniqueness => true
   def user_with_proficiency(rating)
-
+    rating = Rating.where(skill_id: self.id, proficiency_rating: rating).first
+    User.find(rating.user_id)
   end
 end
